597482374accd07209fac6be139bbd10fb6f1436
Manolo <mmgmolina03@gmail.com>
2023-05-06 14:06:44 +0200

    creacion de base de datos terminada y ya testeada

--- src/main/java/com/salesianostriana/dam/principioproyecto/repository/IReformaRepository.java
+++ src/main/java/com/salesianostriana/dam/principioproyecto/repository/IReformaRepository.java
@@ -2,6 +2,7 @@ package com.salesianostriana.dam.principioproyecto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import com.salesianostriana.dam.principioproyectofinal.model.Reforma;
 
 public interface IReformaRepository extends JpaRepository<Reforma, Long>{
--- src/main/java/com/salesianostriana/dam/principioproyectofinal/controller/ClienteController.java
+++ src/main/java/com/salesianostriana/dam/principioproyectofinal/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.dam.principioproyectofinal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.principioproyectofinal.model.Cliente;
+import com.salesianostriana.dam.principioproyectofinal.service.ClienteService;
 
 @Controller
 public class ClienteController {
-
+	
+	@Autowired
+	private ClienteService clienteServicio;
+	
+	@GetMapping("/listaCliente")
+	public String listadoClientes(Model model) {
+		model.addAttribute("listaClientes", clienteServicio.findAll());
+		return "listaClientes";
+	}
 	@GetMapping("/cliente")
 	public String showClienteForm (Model model) {
 		Cliente cliente= new Cliente();
