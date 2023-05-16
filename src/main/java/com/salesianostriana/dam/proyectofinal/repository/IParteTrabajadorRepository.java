c10c5648a11042a124d28e7e33d5e0af3311e4f1
Manolo <mmgmolina03@gmail.com>
2023-05-16 20:23:02 +0200

    asociacion con atributos extra implementado

---
+++ src/main/java/com/salesianostriana/dam/proyectofinal/repository/IParteTrabajadorRepository.java
@@ -0,0 +1,10 @@
+package com.salesianostriana.dam.proyectofinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salesianostriana.dam.proyectofinal.model.ParteTrabajador;
+import com.salesianostriana.dam.proyectofinal.model.ParteTrabajadorPK;
+
+public interface IParteTrabajadorRepository extends JpaRepository<ParteTrabajador, ParteTrabajadorPK>{
+
+}
---
+++ src/main/java/com/salesianostriana/dam/proyectofinal/service/ParteTrabajadorService.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.proyectofinal.service;
+
+import com.salesianostriana.dam.proyectofinal.model.ParteTrabajador;
+import com.salesianostriana.dam.proyectofinal.model.ParteTrabajadorPK;
+import com.salesianostriana.dam.proyectofinal.repository.IParteTrabajadorRepository;
+
+public class ParteTrabajadorService extends BaseService<ParteTrabajador, ParteTrabajadorPK, IParteTrabajadorRepository>{
+
+}
