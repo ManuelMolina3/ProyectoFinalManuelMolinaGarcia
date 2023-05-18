9f7ff0b96d93e84c65be56809e611bc1d1cb922a
admin <mmgmolina03@gmail.com>
2023-05-18 14:29:24 +0200

    intento en completar el buscador

--- src/main/java/com/salesianostriana/dam/proyectofinal/repository/IClienteRepository.java
+++ src/main/java/com/salesianostriana/dam/proyectofinal/repository/IClienteRepository.java
@@ -10,6 +10,12 @@ import com.salesianostriana.dam.proyectofinal.model.Cliente;
 
 public interface IClienteRepository extends JpaRepository<Cliente, Long>{
 
-	@Query("SELECT c FROM Cliente c WHERE CONCAT(c.id_cliente, c.nombre, c.apellido, c.dni, c.email) LIKE %?1%")
+	@Query("""
+			select c from Cliente c where c.id_cliente LIKE %?1%
+			or c.nombre LIKE %?1%
+			or c.apellido LIKE %?1%
+			or c.dni LIKE %?1%
+			or c.email LIKE %?1%
+					""")
 	public List<Cliente> findByAllContainingIgnoreCase(String busqueda);
 }
--- src/main/java/com/salesianostriana/dam/proyectofinal/service/ClienteService.java
+++ src/main/java/com/salesianostriana/dam/proyectofinal/service/ClienteService.java
@@ -2,7 +2,6 @@ package com.salesianostriana.dam.proyectofinal.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.proyectofinal.model.Cliente;
@@ -11,13 +10,12 @@ import com.salesianostriana.dam.proyectofinal.repository.IClienteRepository;
 @Service
 public class ClienteService extends BaseService<Cliente, Long, IClienteRepository>{
 
-	@Autowired
-	IClienteRepository repo;
+
 
 	
 	public List<Cliente> findByClientes(String busqueda){
 
-		return repo.findByAllContainingIgnoreCase(busqueda);
+		return this.repositorio.findByAllContainingIgnoreCase(busqueda);
 
 	}
 	
