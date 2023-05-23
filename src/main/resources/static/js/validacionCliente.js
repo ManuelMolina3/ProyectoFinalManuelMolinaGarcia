768ebafafe96553adbace2832ff80197287f6ff3
admin <mmgmolina03@gmail.com>
2023-05-23 12:02:36 +0200

    paso en diferentes campos

--- src/main/resources/static/js/validacionCliente.js
+++ src/main/resources/static/js/validacionCliente.js
@@ -6,47 +6,48 @@ let telefonoInput = document.getElementById("telefono");
 let emailInput = document.getElementById("email");
 
 // Crear función de validación
-function validarFormulario() {
+function validarFormulario(evento) {
+	let resultado= true;
   // Validar nombre
   if (nombreInput.value.length > 13) {
     alert("El nombre no puede tener más de 13 letras.");
-    return false;
+	resultado= false;
   }
 
   // Validar apellido
   if (apellidoInput.value.length > 13) {
     alert("El apellido no puede tener más de 13 letras.");
-    return false;
+     resultado= false;
   }
 
   // Validar DNI
   let dniRegex = /^[0-9]{8}[a-zA-Z]$/;
   if (!dniRegex.test(dniInput.value)) {
     alert("El DNI no es válido.");
-    return false;
+     resultado= false;
   }
 
   // Validar ciudad
   if (ciudadInput.value.length < 3 || ciudadInput.value.length > 13) {
     alert("La ciudad debe tener entre 3 y 13 letras.");
-    return false;
+     resultado= false;
   }
 
   // Validar teléfono
-  if (telefonoInput.value.length > 9) {
-    alert("El teléfono no puede tener más de 9 números.");
-    return false;
+  if (telefonoInput.value.length = 9) {
+    alert("El teléfono tiene que tener 9 números.");
+     resultado= false;
   }
 
   // Validar email
-let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+gmail\.com$/;
+let emailRegex = /^[^\s@]+@[^\s@]+\@]+gmail\.com$/;
   if (!emailRegex.test(emailInput.value)) {
-    alert("El email debe terminar en @gmail.com.");
-    return false;
+    alert("El email debe terminar en @gmail.com");
+     resultado= false;
   }
 
-
-  return true;
+  evento.preventDefault();
+  return resultado;
 }
 
 let formulario = document.getElementById("formulario");
