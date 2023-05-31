83eaac9ce3434f1fe737c118b9971422873288d2
admin <mmgmolina03@gmail.com>
2023-05-31 11:01:02 +0200

    mejoras en la validacionç

---
+++ src/main/resources/static/js/validacionParte.js
@@ -0,0 +1,31 @@
+document.getElementById("numHoras").addEventListener("blur", validarParte);
+document.getElementById("numHorasExtra").addEventListener("blur", validarParte);
+let error = document.querySelectorAll(".errorForm");
+error.forEach(p => p.hidden = true);
+function validarParte(evento) {
+    let numHorasInput = document.getElementById("numHoras");
+    let numHorasExtraInput = document.getElementById("numHorasExtra");
+    let numHoras = numHorasInput.value;
+    let numHorasExtra = numHorasExtraInput.value;
+    let resultado = true;
+    if (numHoras < 0) {
+        document.getElementById("numHoras").nextElementSibling.hidden = false;
+        resultado = false;
+    }
+    else {
+        document.getElementById("numHoras").nextElementSibling.hidden = true;
+    }
+    if (numHorasExtra < 0) {
+        document.getElementById("numHorasExtra").nextElementSibling.hidden = false;
+        resultado = false;
+    }
+    else {
+        document.getElementById("numHorasExtra").nextElementSibling.hidden = true;
+    }
+    if (!resultado) {
+        evento.preventDefault();
+    }
+    return resultado;
+}
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", validarParte);
