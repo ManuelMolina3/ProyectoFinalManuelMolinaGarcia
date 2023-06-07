ebe3cf8ab516a2a920fe8c52aa12e8d7f502f41e
admin <mmgmolina03@gmail.com>
2023-06-07 13:34:09 +0200

    Cambios en el venta controller importantes para empezar a diseñar las pagina y unir el todo

--- src/main/java/com/salesianostriana/dam/proyectofinal/controller/VentaController.java
+++ src/main/java/com/salesianostriana/dam/proyectofinal/controller/VentaController.java
@@ -1,16 +1,25 @@
 package com.salesianostriana.dam.proyectofinal.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.proyectofinal.excepcion.ExcepcionCarritoVacio;
+import com.salesianostriana.dam.proyectofinal.model.Reforma;
+import com.salesianostriana.dam.proyectofinal.model.Venta;
 import com.salesianostriana.dam.proyectofinal.service.MaterialesService;
 import com.salesianostriana.dam.proyectofinal.service.VentaService;
 
 @Controller
+@RequestMapping("/admin")
 public class VentaController {
 
 	@Autowired
@@ -43,6 +52,27 @@ public class VentaController {
 		servicioDeVenta.removeMateriales(servicioMateriales.findById(id));
 		return "redirect:/carrito";
 	}
+	@ModelAttribute("totalCarrito")
+	public Double mostrartotalCarrito () {   	
+		return servicioDeVenta.totalCarrito();
+	}
+	@PostMapping("/carrito/finalizarCompra")
+	public String finalizarCompra(@AuthenticationPrincipal Reforma reforma) {
+		servicioDeVenta.crearReformaMate(reforma);
+		return "reformaFinalizada";
+	}
 	
-	
+	@ModelAttribute("totalReformas")
+	private double calcularTotalRefomas (List<Venta> ventas) {
+	    double total = 0.0;
+	    for (Venta venta : ventas) {
+	        total += venta.getTotal();
+	    }
+	    return total;
+	}
+	@GetMapping("/venta")
+	public String listVenta(Model model) {
+		model.addAttribute("listaVentas", servicioDeVenta.findAll());
+		return "admin/listVenta";
+	}
 }
